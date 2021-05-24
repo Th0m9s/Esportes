0c30f86a29bf1350bdc1af0853d248edca87d911
Thomas Polvora <thomaspolvorars@hotmail.com>
2021-05-24 19:12:33 -0300

    Commit inicial

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports ={
+    dialect:"postgres",
+    host:"localhost",
+    port:"5432",
+    username:"postgres",
+    password:"kiara170902",
+    database:"esportes",
+    define:{
+        timestemps:true
+    }
+}
